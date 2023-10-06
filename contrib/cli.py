2302d5b1be456fd228e5009355550b49de9a9d7e
Subin Kim <32478597+sudosubin@users.noreply.github.com>
2023-10-07 02:06:25 +0900

    feat: Use `rye` as package manager (#2)
    * feat: Migrate from hatch to rye
    
    * feat: Migrate test configuration to tox
    
    * fix: Fix ci/cd pipelines
    
    * feat: Bump version 0.1.1 -> 0.1.2
    
    * chore: Remove default language version which is unused
    
    * chore: Update hatchling build to exclude tests directory
    
    * chore: Remove cache configuration for rye
    
    * chore: Split sync and validate
    
    * feat: Add requirements lock files
    
    * chore: Add cache for package-ci pipeline

--- contrib/cli.py
+++ contrib/cli.py
@@ -20,12 +20,8 @@ app.add_typer(workspaces, name="workspaces")
 def workspace(
     workspace_name: str,
     arguments: typing.List[str],
-    filter_paths: typing.Annotated[
-        bool,
-        typer.Option(help="A flag to filter paths from arguments for mono repository."),
-    ] = False,
 ) -> None:
-    code = run_in_workspace(workspace_name, arguments, filter_paths=filter_paths)
+    code = run_in_workspace(workspace_name, arguments)
     raise typer.Exit(code=code)
 
 
@@ -35,14 +31,10 @@ def workspace(
 )
 def foreach(
     arguments: typing.List[str],
-    filter_paths: typing.Annotated[
-        bool,
-        typer.Option(help="A flag to filter paths from arguments for mono repository."),
-    ] = False,
 ) -> None:
     code = 0
     for workspace in get_workspaces():
-        exit_code = run_in_workspace(workspace, arguments, filter_paths=filter_paths)
+        exit_code = run_in_workspace(workspace, arguments)
         sys.stdout.write(os.linesep)
         code = exit_code if exit_code != 0 else code
 
@@ -66,45 +58,17 @@ def get_workspace_dir(workspace_name: str) -> str:
     return str(Path(__file__).parent.parent / f"packages/{workspace_name}")
 
 
-def run_in_workspace(
-    workspace_name: str, arguments: typing.List[str], *, filter_paths: bool
-) -> int:
+def run_in_workspace(workspace_name: str, arguments: typing.List[str]) -> int:
     workspace_dir = get_workspace_dir(workspace_name)
 
     if not Path(f"{workspace_dir}/pyproject.toml").exists():
         rich.print(f"[red]➤[/red] {workspace_name} not found")
         return 1
 
-    if filter_paths:
-        arguments, ok = filter_paths_from_arguments(workspace_name, arguments)
-        if not ok:
-            rich.print(f"[yellow]➤[/yellow] {workspace_name} no files to run")
-            return 0
-
     rich.print(f"[blue]➤[/blue] {workspace_name}\n")
-    process = subprocess.run(["hatch", *arguments], cwd=workspace_dir)
+    process = subprocess.run(["rye", *arguments], cwd=workspace_dir)
     return process.returncode
 
 
-def filter_paths_from_arguments(
-    workspace_name: str, arguments: typing.List[str]
-) -> typing.Tuple[typing.List[str], bool]:
-    ok = False
-    workspace_dir = get_workspace_dir(workspace_name) + "/"
-    filtered_arguments = []
-
-    for argument in arguments[1:]:
-        resolved_path = Path(argument).resolve()
-        if resolved_path.exists():
-            resolved = str(resolved_path)
-            if resolved.startswith(workspace_dir):
-                ok = True
-                filtered_arguments.append(resolved.replace(workspace_dir, ""))
-        else:
-            filtered_arguments.append(argument)
-
-    return [arguments[0], *filtered_arguments], ok
-
-
 if __name__ == "__main__":
-    app(prog_name="hatch run")
+    app(prog_name="rye run")
